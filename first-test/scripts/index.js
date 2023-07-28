36c680362a1537d2dc3583b04b1e638338b5eb3f
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2023-07-27 22:32:41 -0300

    feat: :sparkles: - toggle the paragraph text on form submission.

--- first-test/scripts/index.js
+++ first-test/scripts/index.js
@@ -4,5 +4,15 @@ formElement.addEventListener("submit", (e) => handleFormSubmission(e));
 
 function handleFormSubmission(e) {
   e.preventDefault();
+  toggleText();
 }
 
+function toggleText() {
+  const textElement = document.getElementById("text");
+  const defaultText = "This is the <strong>default</strong> text"
+  const newText = "This is the <strong>new</strong> text"
+
+  if (textElement.innerHTML === defaultText) {
+    textElement.innerHTML = newText;
+  } else textElement.innerHTML = defaultText;
+}
