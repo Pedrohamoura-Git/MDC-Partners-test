b9e14aba72ff9dc3f6f04617b8c6d7018bdaf8ef
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2023-07-31 14:34:18 -0300

    feat(test-4): :sparkles: - add an global message to notify the user when the form was successfully submitted, but it can be used in other scenarios as well.

--- forth-test/scripts/index.js
+++ forth-test/scripts/index.js
@@ -27,6 +27,7 @@ function validateForm(e) {
   toggleInputOutlineClass(nameInput, "outline-success");
   toggleInputOutlineClass(emailInput, "outline-success");
   toggleInputOutlineClass(messageInput, "outline-success");
+  showGlobalMessage('Success!', 'outline-success');
 }
 
 function isTextEmpty(text) {
@@ -61,9 +62,14 @@ function toggleInputOutlineClass(el, className) {
 
 function showErrorMessage(element, message) {
   element.nextSibling.nextSibling.innerText = message;
-  element.nextSibling.nextSibling.classList.add('show-error-message');
 
+function showGlobalMessage(message, className) {
+  const globalMessage = document.querySelector("#global-message");
+  globalMessage.innerText = message;  
+  globalMessage.classList.add('global-message-active', className);
+  
   setTimeout(() => {
-    element.nextSibling.nextSibling.classList.remove('show-error-message');
-  }, 3500)
+    globalMessage.classList.remove('global-message-active', className);
+    globalMessage.innerText = '';
+  }, 3500);
 }
